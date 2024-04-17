2cc8ff57793997b2c57ffff0f7eba43ecfb86657
Dada <daviddada360@gmail.com>
2024-04-17 09:11:25 +0100

    doctors

--- resources/views/doctors/signin.blade.php
+++ resources/views/doctors/signin.blade.php
@@ -31,8 +31,8 @@
         </label>
       </div>
       <button type="submit" class="btn btn-primary">Sign in</button>
-      <button type="submit" class="btn btn-primary" href="{{route('doctors.signup')}}">Sign Up</button>
     </form>
+    <button type="submit" class="btn btn-primary" href="{{route('doctors.signup')}}">Sign Up</button>
   </div>
 </body>
 </html>
