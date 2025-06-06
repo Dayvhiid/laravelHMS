203427f5ea55159e1c465e30c28909b52273a4f4
Dada <daviddada360@gmail.com>
2025-06-06 19:27:01 +0100

    NPM issues

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.js',
-                'resources/css/app.css'
+                'resources/css/app.css',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
@@ -22,8 +22,11 @@ export default defineConfig({
     ],
     server: {
         host: '0.0.0.0',
+        port: 5173,
         hmr: {
             host: 'localhost',
+            protocol: 'ws',
         },
+        strictPort: true,
     },
 });
